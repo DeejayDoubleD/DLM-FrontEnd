f383800e1f66857c3f77ecabb8fd3b59dc7375d3
DeejayDoubleD <janzendaniel2002@gmail.com>
2024-02-11 10:42:21 +0100

    added login view and axios

---
+++ src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+    <main>
+        <h1>Login</h1>
+      <form>
+        <label for="username">Nutzername:</label><br>
+        <input type="text" id="username" name="Username"><br>
+        <label for="password">Passwort:</label><br>
+        <input type="password" id="password" name="Password">
+        <input type="submit" id="submitlogin" name="Submitlogin">
+      </form>
+    </main>
+  </template>
